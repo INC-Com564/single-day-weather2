a678fc19cfe91dc778574a3739eac64500613b51
INC-Com739 <Iamisaiahcleveland@gmail.com>
2025-08-25 18:31:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Counter from './Weather'
+
+function App() {
+  
+  return (<>
+      <header>
+        <h1>Weather App</h1>
+      </header>
+      <main>
+        <Counter />
+      </main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/Weather.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function Counter(){
+    const [count, setCount] = useState(0)
+    return(
+        <>
+        <div>
+            <button onClick={() => setCount(count -1)}>Subtract</button>
+            <span>{count}</span>
+            <button onClick={() => setCount(count +1)}>Add</button>
+        </div>
+        </>
+    )
+}
