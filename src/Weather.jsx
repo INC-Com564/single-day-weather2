ee5688801a5c97a6dffde0c21a41dbaf0a7e775d
INC-Com739 <Iamisaiahcleveland@gmail.com>
2025-08-27 20:36:50 -0500

    Netify test

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -3,9 +3,24 @@ import { useState, useEffect } from "react";
 export default function Weather(){
     const [city, setcity] = useState('Zocca');
     const [WeatherData, setWeatherData] = useState(null);
+    const [loading, setloading] = useState(false);
+    const [Error, setError] = useState({message: "", status: false});
 
+
+    function weatherDisplay(){
+       if(WeatherData){
+        return(
+            <div id="weather-div">
+                <p>Description <span>{WeatherData.weather[0].description}</span></p>
+                <p>Icon <span>{WeatherData.weather[0].icon}</span></p>
+                <p>Feels like <span>{WeatherData.main.feels_like}</span></p>
+            </div>
+        )
+       }
+    }
     useEffect(() => {
         async function getWeather() {
+            setloading(true)
             const key = import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY;
                 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
             try {
@@ -18,8 +33,9 @@ export default function Weather(){
                 console.log(result);
             } catch (error) {
                 console.error(error)
+                setError({message: error.message, status: true});
             } finally {
-
+              setloading(false);
             }
         }
         getWeather();
@@ -27,8 +43,12 @@ export default function Weather(){
     return(
         <>
         <section>
-            <h2>Current Weather</h2>
-            {WeatherData && Array.isArray(WeatherData.weather) && WeatherData.weather[0]?.main}
+            <h2>Current Weather for {city}</h2>
+
+            {loading && <p>Loading...</p>}
+
+            {Error.status && <p>ERROR ERROR... Weather App Down! {Error.message}</p>}
+            {WeatherData && weatherDisplay()}
             {/* <form>
                 <label htmlFor="city">Enter Name of City</label>
                 <input type="text" name="city" id="city"></input>
